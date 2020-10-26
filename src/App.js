8ceb4458d5b8d6f6c8b3bfeb873172e1b65d5292
Min KyungHyun <khmin1104@gmail.com>
2020-10-27 02:14:57 +0900

    class component - state & life cycle

--- src/App.js
+++ src/App.js
@@ -1,50 +1,41 @@
+import React from "react"
 import PropTypes from "prop-types"
 
-function Food({name, picture, rating}) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>
-    </div>
-  );
-}
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: "라면",
-    rate: 4.8,
-    image: "https://mblogthumb-phinf.pstatic.net/MjAxODA3MThfNjAg/MDAxNTMxOTAxNjU5Mjk2.TPvVTTjaTNDvIWUXB3Pwq6TeBvES4QBAmxgGmOSLaE4g.UshYkHzPL6LkBQrJzxwyQ17fhudRyV8gbbY75eootggg.PNG.daishin_blog/%25ED%2595%25B4%25EC%2599%25B8_%25EC%259D%25B8%25EA%25B8%25B0_%25ED%2595%259C%25EA%25B5%25AD_%25EC%259D%258C%25EC%258B%259D_01.png?type=w800"
-  },
-  {
-    id: 2,
-    name: "족발",
-    rate: 4.5,
-    image: "https://www.brcn.go.kr/prog/tour_restaurant/tour/sub04_03/restaurantImage_down.do?rstrntCode=157"
-  },
-  {
-    id: 3,
-    name: "김밥",
-    rate: 4.9,
-    image: "https://i.pinimg.com/originals/62/0b/72/620b727a629b7a8d525858299a1b20e4.jpg"
+class App extends React.Component {
+  state = {
+    count: 0
   }
-]
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello!!</h1>
-      {foodILike.map(food=> {
-        return <Food key={food.id} name={food.name} picture={food.image} rating={food.rate} />
-      })}
-    </div>
-  );
+  add = ()=> {
+    // state는 readonly field이다. 
+
+    // setState을 통해 state을 바꿔야 한다.
+    // setState에서 render를 다시 실해하도록 한다.
+    
+    // 또한 setState에서는 state을 비동기적으로 바꾼다.
+    // 그래서 동기화가 안된 데이터를 안사용하도록  직접사용하지 말고 lamda 식을 통해 업데이트할 현재 state을 받아야 한다. 
+    this.setState(current => ({count: current.count+1}))
+  }
+  minus = ()=> {
+    this.setState(current => ({count: current.count-1}))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>I'm a class {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+
+  }
+  componentDidUpdate() {
+
+  }
 }
 
-export default App;
+export default App
