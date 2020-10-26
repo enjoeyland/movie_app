a551b8fb170654e9ed8f89838d7773d236b1a2e3
Min KyungHyun <khmin1104@gmail.com>
2020-10-26 22:15:15 +0900

    Getting arguments  on component.
    ES6에서 부터 생긴 {arg} 사용
    component를 완전하나의 태그 처럼 사용 (JSX 기술)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Potato from './Potato';
+function Food({fav}) {
+  console.log(fav);
+  return <h3>I like {fav}</h3>
+}
 
 function App() {
   return (
     <div className="App">
       <h1>hello!!</h1>
-      <Potato/>
+      <Food fav="kimchi"/>
+      <Food fav="라면"/>
+      <Food fav="삼겹살"/>
+      <Food fav="쭈꾸미"/>
     </div>
   );
 }
